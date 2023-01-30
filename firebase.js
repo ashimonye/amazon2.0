36f3fcade4e1328983a4a57b3200582a63aa45d7
Gideon Nwankwo <uberposeidonn@gmail.com>
2023-01-30 03:27:42 +0100

    firebase commit and css

---
+++ firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB6EPaQOv1I0s-sf06sXncr14Ut5oYwue8",
+    authDomain: "aluusi-20.firebaseapp.com",
+    projectId: "aluusi-20",
+    storageBucket: "aluusi-20.appspot.com",
+    messagingSenderId: "500116747692",
+    appId: "1:500116747692:web:f9ac696a66f0614e4d7609",
+    measurementId: "G-YGXWL7ZRG2"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
